abf1dbba6dfa4ed32ae3ca3a115aaa68507ef6b2
dkiriinya <dkiriinya06sc@gmail.com>
2024-04-21 18:41:00 +0300

    made changes to root

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
